38933a457243683f0566653aacf08fc97595fad4
Yog234 <yogmorin@hotmail.com>
2017-09-16 14:57:47 -0400

    cube moving

---
+++ Vikings/Assets/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.gameObject.transform.Translate(-2, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.gameObject.transform.Translate(0, -2, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.gameObject.transform.Translate(2, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.gameObject.transform.Translate(0, 2, 0);
+        }
+    }
+}
